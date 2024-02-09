840c6d5dcde1d4fa026bc4e126ff5c3b1265396b
Kouyang07 <154615729+Kouyang07@users.noreply.github.com>
2024-02-08 21:42:09 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          One4AllFoundation.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

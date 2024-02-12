4bb9658a4558869bf56fd78cdca4ae10d84895f8
Kouyang07 <154615729+Kouyang07@users.noreply.github.com>
2024-02-11 21:48:49 -0500

    Add files via upload

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,5 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import SingleTeam from "@/components/Team/SingleTeam";
+import teamData from "@/components/Team/teamsData";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
 import { Metadata } from "next";
@@ -10,17 +10,30 @@ export const metadata: Metadata = {
   // other metadata
 };
 
-const AboutPage = () => {
+const Team = () => {
   return (
     <>
       <Breadcrumb
-        pageName="About Page"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        pageName="Team"
+        description="Meet our amazing team members who work tirelessly to make a difference in people's lives."
       />
-      <AboutSectionOne />
-      <AboutSectionTwo />
+
+      <section className="pb-[120px] pt-[120px]">
+        <div className="container">
+          <div className="-mx-4 flex flex-wrap justify-center">
+            {teamData.map((team) => (
+              <div
+                key={team.id}
+                className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
+              >
+                <SingleTeam team={team} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default AboutPage;
+export default Team;
